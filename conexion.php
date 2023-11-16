8be2a09a29d1ab29fc57bd9b961bcc2ee7d59183
Inma0711 <inmagonzalez045@gmail.com>
2023-11-16 18:40:28 +0100

    Actualizar

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     $_servidor ='localhost'; // Esto es para almacenar la ip
     $_usuario = 'root';
-    $_contrasena = 'medac';
+    $_contrasena = 'Inma12345';
     $_base_de_datos = 'db_login';
 
     $conexion = new Mysqli($_servidor, $_usuario, $_contrasena, $_base_de_datos)
